8ec339188dd8a0f84757c7eb965a6fa131224559
Ayushi Maurya <106837615+am-298@users.noreply.github.com>
2025-10-25 16:48:57 +0530

    Update mux.v

--- Circuits/Combinational_Circuits/Multiplexers/mux.v
+++ Circuits/Combinational_Circuits/Multiplexers/mux.v
@@ -1,1 +1,13 @@
 
+module top_module( 
+    input a, b, sel,
+    output out ); 
+    
+    always @(*) begin
+        case (sel)
+            0 : out = a;
+            1 : out =b;
+        endcase
+    end
+
+endmodule
